1dbeac6df37eeca0f71ff06fe1d87212fd24e873
Samuel Pilon <pilon.samuel@hotmail.com>
2017-03-06 20:59:41 -0500

    Re-init of playerController

---
+++ Veritas/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// PlayerController script must be added to the player GameObject
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody2D body;
+
+    private Vector2 positionTo;
+
+	void Start () {
+        body = GetComponent<Rigidbody2D>();
+        positionTo = body.position;
+        Spawn();
+	}
+	
+    // Listen for clicks to move the player around
+	void Update () {
+        if (Input.GetMouseButton(0)) {
+            Vector3 clicked = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            positionTo = new Vector2(clicked.x, clicked.y);
+        }
+        body.position = Vector2.MoveTowards(body.position, positionTo, Time.deltaTime*3f);
+	}
+
+    // Method that Spawn the player somewhere on the map
+    void Spawn() {
+
+    }
+}
