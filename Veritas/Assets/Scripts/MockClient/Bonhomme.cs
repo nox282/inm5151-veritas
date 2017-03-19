cb4258c4405abd90eaabd3e821228a4ff095999c
Giulia Cantoni <cantoni.giulia@courrier.uqam.com>
2017-03-19 17:33:08 -0400

    Reading Json. Not communicating with server yet.

---
+++ Veritas/Assets/Scripts/MockClient/Bonhomme.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class Bonhomme {
+
+    public string name;
+    public int funlevel;
+    public string color;
+
+    //public string Name { get { return name; } }
+    //public int Funlevel { get { return funlevel; } }
+    //public string Color { get { return color; } }
+}
+
+
--- Veritas/Assets/Scripts/MockClient/Client.cs
+++ Veritas/Assets/Scripts/MockClient/Client.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class Client : MonoBehaviour {
@@ -15,6 +16,35 @@ public class Client : MonoBehaviour {
 	}
 
     private void OnMouseDown() {
-        Debug.Log("clic!"); 
+        //Debug.Log("clic!");
+        string jsonstr = ReadJsonFile(Application.dataPath + "/Scripts/MockClient/" + "TestData.json");
+        //POST(jsonstr); 
+        
     }
+
+    private string ReadJsonFile(string jsonfile) {
+        Bonhomme myBonhomme = new Bonhomme(); 
+
+        string jsondata = File.ReadAllText(jsonfile);
+
+        myBonhomme = JsonUtility.FromJson<Bonhomme>(jsondata); 
+
+        Debug.Log(jsondata);
+        Debug.Log(myBonhomme.name+", "+myBonhomme.funlevel+", "+myBonhomme.color);
+
+        return jsondata; 
+    }
+
+    /*public WWW POST(string jsonstr) {
+        WWW www;
+        Hashtable postHeader = new Hashtable();
+        postHeader.Add("Content-Type", "application/json");
+
+        // convert json string to byte
+        var formData = System.Text.Encoding.UTF8.GetBytes(jsonstr);
+
+        www = new WWW(POSTAddUserURL, formData, postHeader);
+        StartCoroutine(WaitForRequest(www));
+        return www;
+    }*/
 }
