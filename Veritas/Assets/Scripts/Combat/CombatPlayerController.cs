5a0976e75f399523091962205989d045e402ae81
Nicolas Hamard <nox.tux@gmail.com>
2017-04-24 02:34:17 -0400

    adding new animations and sprites in the right branch

---
+++ Veritas/Assets/Scripts/Combat/CombatPlayerController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatPlayerController : MonoBehaviour {
+
+    public int HP;
+
+    public void hit(){
+        HP -= 1;
+    }
+    public bool isDead(){
+        return HP <= 0;
+    }
+}
---
+++ Veritas/Assets/Scripts/Combat/CombatUIController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CombatUIController : MonoBehaviour {
+
+    public CombatManager cm;
+    public Text question;
+    public InputField reponse;
+    public Button valider;
+    public Button effacer;
+
+	void Start () {
+		question.text = cm.GetQuestion();
+        valider.onClick.AddListener(delegate{
+            validate();
+        });
+        effacer.onClick.AddListener(delegate{
+            erase();
+        });
+	}
+	
+    void validate(){
+        cm.TryAnswer(reponse.text);
+    }
+
+    void erase(){
+        reponse.text = "";
+    }
+}
